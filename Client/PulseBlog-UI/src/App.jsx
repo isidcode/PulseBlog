63a66b63add69717f006ae6f5d6c535999d7ee18
rakesh <014rakeshofficial@gmail.com>
2026-03-02 19:23:34 +0530

    Client and Server Folder Structure

---
+++ Client/PulseBlog-UI/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="flex h-screen items-center justify-center bg-brand-primary">
+        <h1 className="text-4xl font-bold text-white">PulseBlog is Ready</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
