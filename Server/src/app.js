af7478d1ea889507c1275ed7f8121e40f4d83d72
rakesh <014rakeshofficial@gmail.com>
2026-03-04 11:11:58 +0530

    App & Builtin Middlewares  configured.

---
+++ Server/src/app.js
@@ -0,0 +1,26 @@
+import dotenv from "dotenv";
+dotenv.config()
+import express from 'express';
+
+const app = express()
+
+//to clean cookie string in to a clean object
+app.use(cookieParser())
+
+//for valid request access
+app.use(cors())
+
+//configure server to accept data up to 16kb
+app.use(express.json({limit:"16kb"}))
+
+//configure server to clean garbage value from url convert in to req.body
+app.use(express(urlencoded({extended:"true",limit:"16kb"})))
+
+//configure server to check public folder before complex db query
+app.use(express.static("public"))
+
+//day1 till here
+
+
+
+export {app}
